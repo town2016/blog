ad6d3f73adc18d5b8da1a20bf07c738055f1d95b
taowennan <taowennanwj65920@touna.cn>
2018-09-12 09:59:23 +0800

    bug修复

--- public/js/table.js
+++ public/js/table.js
@@ -6,47 +6,101 @@ function Table (model, data, el) {
     tableData: data || [],
     parent: el,
     create: function () {
-      let html = `<table id="${this.id}">
-                    <thead>
-                      <tr>${this.createThead()}<tr>
-                    </thead>
-                    <tbody>${this.createTbody()}</tbody>
-                  </table>`;
-      el.innerHTML = html
+      let table = document.createElement('table')
+      table.id = this.id
+      table.appendChild(this.createThead())
+      table.appendChild(this.createTbody())
+      el.appendChild(table)
     },
     createThead: createThead,
     createTbody: createTbody,
     createTd: createTd,
-    
+    render: render
   }
 }
 // 创建表头
 function createThead () {
-  let thead = '';
+  var  html = '', thead = createElemetNode('thead'), tr = createElemetNode('tr'); thead.appendChild(tr)
   this.tableModel.map(item => {
-    thead += `<th id="${item.prop}">${item.label}</th>`
+    let th = createElemetNode('th')
+    th.id = item.prop
+    th.innerHTML = item.label
+    if (item.width) th.width = item.width 
+    tr.appendChild(th)
   })
+  thead.appendChild(tr)
   return thead
 }
 // 创建表格主体
 function createTbody () {
-  let tbody = '';
-  this.tableData.map((item, index) => {
-    tbody += `<tr>${this.createTd(item)}</tr>`
+  let tbody = createElemetNode('tbody');
+  if (this.tableData.length === 0) {
+    tbody.innerHTML = `<tr><td colSpan='${this.tableModel.length}' align='center'><font color='#aaa'>暂无数据</font></td><tr>`
+    return tbody
+  }
+  this.tableData.map((row, index) => {
+    var tr = createElemetNode('tr')
+    tbody.appendChild(tr, this.createTd(tr, row))
   })
   return tbody
 }
 // 创建表格
-function createTd (data) {
-  let td = ''
+function createTd (tr, data) {
   this.tableModel.map((item, index) => {
-    if (item.formater) {
-      td += `<td>${item.formater(data[item.prop])}</td>`
+    if (item.formatter) {
+      var td = createElemetNode('td')
+      td.appendChild(item.formatter(this.render, data))
+      tr.appendChild(td)
     } else {
-      td += `<td>${data[item.prop] ? data[item.prop] : ''}</td>`
+      var td = createElemetNode('td', null, data[item.prop] ? data[item.prop] : '')
+      tr.appendChild(td)
     }
   })
-  return td
+  return tr
 }
+
+function createElemetNode (tag, classes = null, innerText) {
+  let elem = document.createElement(tag)
+  if (classes) elem.className = addClass(classes)
+  if (innerText) elem.innerText = innerText
+  return elem
+}
+
 // 自定义render
-function render (el, options, text) {}
+function render (el, options, text) {
+  var elem = createElemetNode(el)
+  // 元素添加class
+  if (options.class) elem.className = addClass(options.class)
+  // 元素写入内容
+  if (text) elem.innerText = text
+  // 元素绑定事件
+  if (options.on) {
+    let events = options.on
+    for (var k in events) {
+      bindEvent(elem, k, events[k])
+    }
+  }
+  // 元素添加子元素
+  if (options.children && options.children.length > 0) {
+    options.children.map(item => {
+      elem.appendChild(item)
+    })
+  }
+  return elem
+}
+
+function addClass (classObj) {
+  let classArr = []
+  for (var k in classObj) {
+    classObj[k] && classArr.push(k)
+  }
+  return classArr.join(' ')
+}
+
+function bindEvent (el, type, fn) {
+  switch (type) {
+    case 'click':
+    eventHandler.addEvent(el, 'click', fn)
+    break
+  }
+}
